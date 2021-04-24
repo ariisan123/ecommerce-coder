4060b781d5a0b4e7cdedaf90802acc27b94736db
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-04-24 19:58:39 -0300

    data for testing

---
+++ src/components/data.js
@@ -0,0 +1,76 @@
+const data = [
+  {
+    id: 1,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  },
+  {
+    id: 2,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  },
+  {
+    id: 3,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  },
+  {
+    id: 4,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  },
+  {
+    id: 5,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  },
+  {
+    id: 6,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  },
+  {
+    id: 7,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  },
+  {
+    id: 8,
+    image:
+      'https://cdn.pocket-lint.com/r/s/1200x/assets/images/155087-laptops-review-microsoft-surface-laptop-go-review-image1-6ezitk9ymj.jpg',
+    price: 238.73,
+    title: 'Titulo 1',
+    description: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut 
+      elit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus`
+  }
+];
+
+export default data;
