1c6f76dfc7f733cc40d93a65d3d6aece14176ae4
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-04-24 21:18:46 -0300

    timeout effect

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import data from '../data';
 import Item from '../Item/Item';
 
 const ItemListContainer = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      setItems([...data]);
+    }, 2000);
+  }, []);
+  console.log(items);
   return (
     <div className="item-list-container">
-      {data.map((item) => (
-        <Item item={item} key={item.id} />
-      ))}
+      {items.length === 0 && <h1>Cargando...</h1>}
+      {items && items.map((item) => <Item item={item} key={item.id} />)}
     </div>
   );
 };
